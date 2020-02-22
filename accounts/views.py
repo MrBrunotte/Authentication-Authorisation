f01fa1d356cb068bdcd081a473f7f62fa1b1846f
mrbrunotte <mrbrunotte@gmail.com>
2020-02-22 20:43:14 +0100

    Add logout url and views

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib import auth
+from django.contrib import auth,messages
 
 # Create your views here.
 def index(request):
@@ -7,6 +7,7 @@ def index(request):
     return render(request, 'index.html')
 
 def logout(request):
-    """ log out the user """
+    """ logout the user """
     auth.logout(request)
+    messages.success(request, "You have successfully been logged out!")
     return redirect(reverse('index'))
