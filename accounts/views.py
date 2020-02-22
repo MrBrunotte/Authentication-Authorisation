bf35cf1cdf3eedccd5a5c5e474b23c0c113ceb86
mrbrunotte <mrbrunotte@gmail.com>
2020-02-22 20:57:01 +0100

    Add login form and views

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib import auth,messages
+from django.contrib import auth, messages
+from accounts.forms import UserLoginForm
 
 # Create your views here.
+
+
 def index(request):
     """return the index.html file """
     return render(request, 'index.html')
+
 
 def logout(request):
     """ logout the user """
@@ -12,6 +16,8 @@ def logout(request):
     messages.success(request, "You have successfully been logged out!")
     return redirect(reverse('index'))
 
+
 def login(request):
     """ Return a login page """
-    return render(request, 'login.html')
+    login_form = UserLoginForm()
+    return render(request, 'login.html', {"login_form": login_form})
